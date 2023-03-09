a24447c341de186391800ca07d95876caf91aaa7
yourfavoriteself <xeniacuzmina@yandex.ru>
2023-03-09 18:05:20 +0300

    bv

--- _build/third-party/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ksenia55/lab05/third-party/gtest/googletest
-# Build directory: /Users/ksenia55/lab05/_build/third-party/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
